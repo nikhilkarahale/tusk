06fe05247d96a1e1da1b9871d013521ea761fb2a
Nikhil Karahale <nikhilkarahale@gmail.com>
2022-04-05 20:06:46 +0530

    Done with current version line

--- ifcondition.py
+++ ifcondition.py
@@ -5,6 +5,4 @@ b = int(input ("Enter 2nd value"))
 c = int(input("Enter 3rd value"))
 print("All values are Equal" if a == b == c else" A is greater value " if a > b and a > c else "B is greator valur" if b > c else "C is grerator value")
 
- 
-  
-  #update file
+#update file
