71bc70ae27e5b77b86104558b1a43ac5d12169e9
Nikhil Karahale <nikhilkarahale@gmail.com>
2022-04-05 19:55:22 +0530

    space changes

--- ifcondition.py
+++ ifcondition.py
@@ -5,4 +5,6 @@ b = int(input ("Enter 2nd value"))
 c = int(input("Enter 3rd value"))
 print("All values are Equal" if a == b == c else" A is greater value " if a > b and a > c else "B is greator valur" if b > c else "C is grerator value")
 
+ 
   
+  #update file
